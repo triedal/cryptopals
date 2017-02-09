08f0769f87d26a3c4f7d1e722ed8f48a1c2afa77
Tyler Riedal <triedal@mail.usf.edu>
2017-02-08 23:23:15 -0500

    oops

--- basics/challenge1.c
+++ basics/challenge1.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
 {
     char *hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
     char *expected = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-    char hex_len = strnlen(hex, 100);
+    int hex_len = strnlen(hex, 100);
     
     // Two hex values per byte of data
     char hex_decoded[(hex_len / 2) + 1];
